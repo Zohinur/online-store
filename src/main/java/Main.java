5219bbb064597c7291e2196257717b54af578742
zohinurmiah <zohinur.m17@gmail.com>
2026-05-08 10:17:25 -0400

    New changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,82 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner myScanner = new Scanner(System.in);
+    static ArrayList<onlineStore> onlineShop = new ArrayList<onlineStore>();
+    static boolean running = true;
+    static String userSelection;
+//    static HashMap<>
+
+    public static void main(String[] args) {
+        String productFile = "src/main/resources/Products.csv";
+        String homeScreen;
+        String prompt;
+
+        displayProduct(productFile);
+        do {
+            prompt = """
+                    1. Display Product
+                    2. Display Cart
+                    3. Exit this application!""";
+            System.out.println(prompt);
+            userSelection = myScanner.nextLine();
+            myScanner.nextLine();
+            if (userSelection.equals("1")) {
+
+            } else if (userSelection.equals("2")) {
+
+            } else if (userSelection.equals("3")) {
+                break;
+            }
+
+        } while (running);
+    }
+
+    public static void displayProduct(String filePath) {
+
+        try (FileReader reader = new FileReader(filePath);
+             BufferedReader buffReader = new BufferedReader(reader);
+        ) {
+            buffReader.readLine();
+            String currentLine;
+            while ((currentLine = buffReader.readLine()) != null) {
+                String[] partProduct = currentLine.split("\\|");
+                String storeSKU = partProduct[0];
+                String storeProductName = partProduct[1];
+                double storePrice = Double.parseDouble(partProduct[2]);
+                String storeDepartment = partProduct[3];
+
+                onlineShop.add(new onlineStore(storeSKU, storeProductName, storePrice, storeDepartment));
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println("File not found: " + ex);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        do  {
+            System.out.println("""
+                    1. search or filter the list of product
+                    2. Add a product to the cart
+                    3. Go back to the home page""");
+            userSelection = myScanner.nextLine();
+            myScanner.nextLine();
+            // HashMap<>
+            if (userSelection.equals("1")) {
+                showProduct();
+            }
+        } while (running);
+
+    }
+
+    public static void showProduct() {
+        for (onlineStore s : onlineShop) {
+            s.displayProduct();
+        }
+    }
 }
--- src/main/java/onlineStore.java
+++ src/main/java/onlineStore.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
-
 public class onlineStore {
+
+    public String SKU;
+    public String productName;
+    public double price;
+    public String department;
+
+    public onlineStore(String SKU, String productName, double price, String department) {
+        this.SKU = SKU;
+        this.productName = productName;
+        this.price = price;
+        this.department = department;
+    }
+    public String getSKU(){
+        return getSKU();
+    }
+
+    public void setSKU(String SKU) {
+        this.SKU = SKU;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price=price;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public void displayProduct() {
+        System.out.printf("%-8s %-35s $%8.2f %-20s%n", SKU, productName, price, department);
+    }
+
 }
